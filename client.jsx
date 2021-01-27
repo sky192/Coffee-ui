6cb65c9e3bb65894a3bb78798fc7c4f3ed050e23
sky192 <samahehloghmani@gmail.com>
2021-01-26 23:58:03 -0500

    created ErrorPage.jsx file

--- client.jsx
+++ client.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
 import CoffeesPage from './pages/Coffees'
+import ErrorPage from './pages/Error'
 
 render(<BrowserRouter>
   <Switch>
     <Route exact path="/" component={CoffeesPage} />
+    <Route path="*" component={ErrorPage} />
   </Switch>
 </BrowserRouter>,
 document.getElementById('root'))
---
+++ pages/Error.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => (
+  <div className="page">
+    <div className="title">A searchable list of all your favorite coffees</div>
+    <div className="subtitle">choose the coffee</div>
+    <div>Sorry, unable to find the page you are looking for</div>
+  </div>
+)
