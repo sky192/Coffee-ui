6b733e400d8e3ab6a5cd0bc9d2ff719e805c9f55
sky192 <samahehloghmani@gmail.com>
2021-01-26 22:09:49 -0500

    install the react-router-dom package.

--- client.jsx
+++ client.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Coffees from './components/Coffees'
 
-render(<Coffees />,
-  document.getElementById('root'))
+render(<BrowserRouter>
+  <Switch>
+    <Route exact path="/" component={Coffees} />
+  </Switch>
+</BrowserRouter>,
+document.getElementById('root'))
